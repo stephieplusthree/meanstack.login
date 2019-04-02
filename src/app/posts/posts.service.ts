874a15106fb8e22ade04a08713562bbae2d34821
github_username <you@domain.com>
2019-04-02 16:57:24 -0700

    post.service.ts registered

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,8 @@
+import { Injectable } from '@angular/core';
+
 import { Post } from './post.model';
 
+@Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
 
