d711984f742e38669dff8afcd74cc3b076e1c319
github_username <you@domain.com>
2019-04-02 07:56:40 -0700

    post in text box out put what you posted in box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  posts = [];
+  storedPosts = [];
 
   onPostAdded(post) {
-    this.posts.push(post);
+    this.storedPosts.push(post);
   }
 }
